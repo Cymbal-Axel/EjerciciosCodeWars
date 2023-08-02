c5cd8f3486d9fa00c832043090239b03cda900d4
Axel <cymbalaxel@gmail.com>
2023-08-02 12:24:26 -0300

    se agrego una forma de buscar el promedio con un forEach

--- find_average.js
+++ find_average.js
@@ -18,3 +18,20 @@ function find_average(array) {
     var promedio = suma / array.length
     return promedio;
   }
+
+
+function average(array){
+  var total = 0;
+  var count = 0;
+
+  array.forEach((item, index)=>{
+    item += total
+    count++
+  });
+  
+  if(total === 0){
+    return total;
+  } else {
+    return total / count;
+  }
+}
